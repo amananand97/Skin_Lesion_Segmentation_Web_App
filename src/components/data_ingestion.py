4aec9b2985b7a8f00ebf3d7281e87425909d4be0
Aman <amananand97@gmail.com>
2024-01-07 18:52:32 +0530

    Added comments

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,8 @@
 import os
 import sys
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTranformation
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -9,25 +12,23 @@ import logging
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.append(project_root)
 
-from src.exception import CustomException
-from src.logger import logging
-from src.components.data_transformation import DataTranformation
-
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
     test_data_path: str=os.path.join('artifacts','test.csv')
-    raw_data_path: str=os.path.join('artifacts','raw.csv')
+    raw_data_path: str=os.path.join('artifacts','data.csv')
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
+        # Updaing the log file
         logging.info("Entered the data ingestion method or component")
         try:
             # Reading the data
             df = pd.read_csv('notebook/data/stud.csv')
+            # Updaing the log file
             logging.info('Imported the data as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
@@ -35,6 +36,7 @@ class DataIngestion:
             # Converting raw data path into csv file
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
+            # Updaing the log file
             logging.info("Train test split initiated")
 
             # Implementing train test split
@@ -46,6 +48,7 @@ class DataIngestion:
             # Saved test set in cvs file
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
+            # Updaing the log file
             logging.info("Ingestion of the data is completed")
 
             return(
--- src/logger.py
+++ src/logger.py
@@ -2,13 +2,15 @@ import logging
 import os
 from datetime import datetime
 
-
+# Creating a log file and using f-string naming file
 LOG_FILE=f"{datetime.now().strftime('%m_%d%_%Y_%H_%M_%S')}.log"
+# Creating Log folder where all the log files will append
 logs_path=os.path.join(os.getcwd(),'logs', LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
+# Creating a basic configuration of log file/format
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format= "[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
