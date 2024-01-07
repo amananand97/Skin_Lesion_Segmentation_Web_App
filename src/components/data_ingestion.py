96e10ab8adf5fc299f364b6c6f1abce7a856b930
Aman <amananand97@gmail.com>
2024-01-07 18:13:27 +0530

    Updated Data ingestion and transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,7 @@ sys.path.append(project_root)
 
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTranformation
 
 @dataclass
 class DataIngestionConfig:
@@ -59,4 +60,6 @@ class DataIngestion:
 if __name__=="__main__":
 
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTranformation()
---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import dill
+import numpy as np
+import pandas as pd
+
+# Add the project root directory to the Python path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.append(project_root)
+
+from src.exception import CustomException
+from src.logger import logging
+
+def save_object(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
